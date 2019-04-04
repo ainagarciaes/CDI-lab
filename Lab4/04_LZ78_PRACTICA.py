622f142bfbb9fd828b6847efb413413ff98cfbc0
Auri <ainaa.105@gmail.com>
2019-04-04 08:10:23 +0200

    finished lab4

--- Lab4/04_LZ78_PRACTICA.py
+++ Lab4/04_LZ78_PRACTICA.py
@@ -16,34 +16,35 @@ LZ78Code(mensaje)=[[0, 'w'], [0, 'a'], [0, 'b'], [3, 'a'],
 """
 
 def LZ78Code(mensaje):
-    n = len(mensaje)
-    codigo = []
-    diccionario = []
+    code = []
+    dic = []
     pos = 0
-    while pos < n:
+    while pos < len(mensaje):
         index = 0
         l = 1
-        cadena = mensaje[pos:pos+l]
-        while True:
-            try:
-                index = diccionario.index(cadena)+1
+        st = mensaje[pos:pos+l]
+        while 1:
+            i = dic.index(st) if st in dic else None
+            if i is not None:
+                index = i + 1
                 l += 1
-                if pos + l > n: break
-                cadena = mensaje[pos:pos+l]
-            except:
+                if pos + l > len(mensaje): 
+                    break
+                st = mensaje[pos:pos+l]
+            else:
                 break
         pos += l
-        if pos < n:
-            diccionario += [cadena]               
-            codigo += [[index,mensaje[pos-1]]]
-        elif pos == n:
-            diccionario += [cadena]
-            codigo += [[index,mensaje[pos-1]]]            
-            codigo += [[0,'EOF']]
+        if pos < len(mensaje):
+            dic += [st]               
+            code += [[index,mensaje[pos-1]]]
+        elif pos == len(mensaje):
+            dic += [st]
+            code += [[index,mensaje[pos-1]]]            
+            code += [[0,'']]
         else:
-            diccionario += [cadena]
-            codigo += [[index,'EOF']]
-    return codigo 
+            dic += [st]
+            code += [[index,'']]
+    return code 
     
     
 """
@@ -59,38 +60,38 @@ LZ78Decode(code)='mississippi mississippi river'
 """    
 
 def LZ78Decode(codigo):
-    m = ''
-    diccionario = []
-    n = len(codigo)
-    for i in range(n-1):
-        index = codigo[i][0]
-        letra = codigo[i][1]
+    dic = []
+    decoded = ''
 
-        if index==0:
-            m += letra
-            diccionario += [letra]
+    for i in range(0, len(codigo)-1):
+        l = codigo[i][1]
+        index = codigo[i][0]
+
+        if index == 0:
+            decoded += l
+            dic += [l]
 
         else:
-            palabra = diccionario[index-1] + letra
-            m += palabra
-            diccionario+=[palabra]
+            word = dic[index-1] + l
+            dic += [word]
+            decoded += word
             
-    index = codigo[n-1][0]
-    letra = codigo[n-1][1]
+    l = codigo[len(codigo)-1][1]
+    index = codigo[len(codigo)-1][0]
 
     if index > 0:
-        palabra = diccionario[index-1]
-        m += palabra
+        word = dic[index-1]
+        decoded += word
       
-    return m
+    return decoded
 
     
     
 mensaje='wabba wabba wabba wabba woo woo woo' 
 mensaje_codificado=LZ78Code(mensaje)
-#print('Código: ',mensaje_codificado)   
+print('Código: ',mensaje_codificado)   
 mensaje_recuperado=LZ78Decode(mensaje_codificado)
-#print('Código: ',mensaje_codificado)   
+print('Código: ',mensaje_codificado)   
 print(mensaje)
 print(mensaje_recuperado)
 if (mensaje!=mensaje_recuperado):
@@ -98,16 +99,16 @@ if (mensaje!=mensaje_recuperado):
 
 mensaje='mississipi mississipi' 
 mensaje_codificado=LZ78Code(mensaje)
-#print('Código: ',mensaje_codificado)   
+print('Código: ',mensaje_codificado)   
 mensaje_recuperado=LZ78Decode(mensaje_codificado)
-#print('Código: ',mensaje_codificado)   
+print('Código: ',mensaje_codificado)   
 print(mensaje)
 print(mensaje_recuperado)
 if (mensaje!=mensaje_recuperado):
         print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 #%%
-'''
+
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
 
 import time
@@ -119,12 +120,11 @@ start_time = time.clock()
 mensaje_recuperado=LZ78Decode(mensaje_codificado)
 print (time.clock() - start_time, "seconds DECODE")
 ratio_compresion=8*len(mensaje)/((bits_indice+8)*len(mensaje_codificado))
-#print(len(mensaje_codificado),ratio_compresion)
+print(len(mensaje_codificado),ratio_compresion)
 if (mensaje!=mensaje_recuperado):
         print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print(len(mensaje),len(mensaje_recuperado))
         print(mensaje[-5:],mensaje_recuperado[-5:])
-'''
 
 
 
